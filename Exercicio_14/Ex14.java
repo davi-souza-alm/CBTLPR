f28794f610da780a03d5acb21b1fe6f5f993e240
Davi de Souza Almeida <95661955+davi-souza-alm@users.noreply.github.com>
2026-03-25 18:18:04 -0300

    Add files via upload

---
+++ Exercicio_14/Ex14.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class Ex14 {
+
+    public static void main(String args[]) {
+        double r, a, VolumeCubo, VolumeEsfera, VolumeLivre;
+
+        Scanner ler = new Scanner(System.in);
+
+        System.out.println("Digite o raio da esfera inscrita: ");
+        r = ler.nextDouble();
+
+        System.out.println("Digite a aresta do cubo: ");
+        a = ler.nextDouble();
+
+        if ((2 * r) > a) {
+            System.out.println("Dados invalidos!");
+            System.out.println("O diametro da esfera nao pode ser maior que a aresta do cubo!");
+        } else {
+            VolumeEsfera = ((4.0 / 3.0) * Math.PI * Math.pow(r, 3));
+
+            VolumeCubo = Math.pow(a, 3);
+
+            VolumeLivre = VolumeCubo - VolumeEsfera;
+
+            System.out.printf("O volume livre do ambiente e igual a %.2f", VolumeLivre);
+        }
+    }
+}
