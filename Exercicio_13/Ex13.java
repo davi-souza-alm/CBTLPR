70f07653c6a5fb5b5b1011804ecf40688363287d
Davi de Souza Almeida <95661955+davi-souza-alm@users.noreply.github.com>
2026-03-25 18:36:01 -0300

    Update Ex13.java

--- Exercicio_13/Ex13.java
+++ Exercicio_13/Ex13.java
@@ -1,3 +1,7 @@
+// Davi de Souza Almeida (CB 3038734) & Gustavo Cardoso Muniz (CB 3037045)
+//13. Calcular e exibir a velocidade final (em km/h) de um automóvel, a partir dos
+//valores da velocidade inicial (em m/s), da aceleração (m/s2) e do tempo de percurso (em s) que serão digitados.
+
 import java.util.Scanner;
 
 public class Ex13{
