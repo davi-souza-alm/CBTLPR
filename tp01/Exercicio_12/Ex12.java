9187bec8823c1020a93bce9876f0e04b90e8069f
Davi de Souza Almeida <95661955+davi-souza-alm@users.noreply.github.com>
2026-06-15 22:18:58 -0300

    Add files via upload

---
+++ tp01/Exercicio_12/Ex12.java
@@ -0,0 +1,23 @@
+// Davi de Souza Almeida (CB 3038734) & Gustavo Cardoso Muniz (CB 3037045)
+// 12. Calcular e exibir o volume de um cone a partir dos valores da altura e do raio da
+// base que serão digitados.
+
+import java.util.Scanner;
+
+public class Ex12{
+	public static void main(String args[]){
+		double altura, raio, volume;
+
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Digite a altura do cone");
+		altura = ler.nextDouble();
+
+		System.out.println("Digite o raio da base do cone");
+		raio = ler.nextDouble();
+
+		volume = (1.0 / 3.0) * Math.PI * (raio * raio) * altura;
+		
+		System.out.printf("O volume do cone e igual a: %.2f ", volume);
+	}
+}
